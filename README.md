86ab4e52722f80d8ca50ed614d3d2ca5d949cfb0
NicolasJoaquinParisi <nicojp.1808@gmail.com>
2022-12-21 09:20:50 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# python-csv-splitter-in-batches
---
+++ csv-splitter.py
@@ -0,0 +1,39 @@
+import csv
+
+csv_name = "products-stocks.csv"
+
+file_name_base = 'split'
+
+rows_to_split = int(input('Enter the number of lines per file: '))
+
+with open(csv_name) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+
+    # Save the row header
+    header = next(csv_reader)
+    
+    # Initializing current lines writted
+    lines = 0
+
+    # The initial batch
+    batch = 1
+
+    file = open('split-' + '1.csv', 'w', newline='')
+    writer = csv.writer(file)
+
+    for row in csv_reader:
+        if (lines == 0):
+            writer.writerow(header)
+
+        writer.writerow(row)
+        lines += 1
+
+        if (lines == rows_to_split):
+            file.close()
+            
+            batch += 1
+
+            file = open(file_name_base + '-' + str(batch) + '.csv', 'w', newline='')
+            writer = csv.writer(file)
+
+            lines = 0
